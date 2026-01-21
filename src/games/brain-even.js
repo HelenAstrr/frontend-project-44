90ad4738f622e03645f779ae775196d070c4de47
Елена П <helenastrr27@yandex.ru>
2026-01-21 12:54:00 +0300

    исправила последнюю ошибку линтера в brain-even.js
    - Убрала лишние скобки в стрелочной функции
    - Правильно: const isEven = num => num % 2 === 0
    - Линтер теперь проходит полностью
    - Все тесты игр уже проходили (26 examples, 0 failures)

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,6 +1,6 @@
 import getRandomNumber from '../utils/random.js'
 
-const isEven = (num) => num % 2 === 0
+const isEven = num => num % 2 === 0
 
 const generateRound = () => {
   const number = getRandomNumber(1, 100)
